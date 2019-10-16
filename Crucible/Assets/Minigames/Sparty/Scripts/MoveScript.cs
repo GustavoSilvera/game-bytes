d4deefc1d272df07abdd5b22aedb143405192f67
Gustavo <gustavo.r.silvera@gmail.com>
2019-10-16 19:21:55 -0400

    fixed double jump bug

--- Crucible/Assets/Minigames/Sparty/Scripts/MoveScript.cs
+++ Crucible/Assets/Minigames/Sparty/Scripts/MoveScript.cs
@@ -17,7 +17,9 @@ public class MoveScript : MonoBehaviour {
 	rect platform;
 	bool double_jump = true;
 	int jump_count = 0;
+	bool up = false;
 	int player = 0;
+	GameObject other;
 	// Use this for initialization
 	void Start () {
 		accel = new Vector3(0,-g);
@@ -34,6 +36,7 @@ public class MoveScript : MonoBehaviour {
 			player = 1;
 			other_player_name = "Player1";
 		}
+		other = GameObject.Find(other_player_name);
 	}
 	float abs(float x){
 		if(x < 0) return -x;
@@ -69,25 +72,41 @@ public class MoveScript : MonoBehaviour {
 		else if (v==0) return 0;
 		return -1;
 	}
-	void collision(bool near_ground){
-		if(!is_in){
-			if (near_ground) vel.x = -(float)(0.8*vel.x);
-			vel.y = -(float)(vel.y);
+	void collision(float ground){
+		if (pos.y < ground + 0.1 || vel.y > 0) {
+			vel.x = -(float)(0.8*vel.x);//deflected
+			//float inside = sign(pos2.x - pos.x)*(1 - abs(pos2.x - pos.x));
+			//if(pos.y > pos2.y)//if(!(pos2.y > pos.y)) 
+			//this.transform.Translate(-inside, 0f, 0f);	//push back a little
 		}
-		else{
-			//separate
+			vel.y = -(float)(vel.y);
+		if(pos.y > ground + 0.5){
+			if(pos.y > pos2.y) this.GetComponent<HealthScript>().TakeDamage(1);
+			//float inside = sign(pos2.y - pos.y)*(1 - abs(pos2.y - pos.y));
+			//if(pos.y > pos2.y)//if(!(pos2.y > pos.y)) 
+			//this.transform.Translate(0f, -inside, 0f);	//push back a little
 		}
 	}
 	// Update is called once per frame
 	void Update () {
 		pos = set_vec2(this.transform.position.x, this.transform.position.y);
-		GameObject other = GameObject.Find(other_player_name);
 		pos2 = set_vec2(other.transform.position.x, other.transform.position.y);
 		joystick = set_vec2(
 			MinigameInputHelper.GetHorizontalAxis(player), 
 			MinigameInputHelper.GetVerticalAxis(player)
 		);
 		float ground = (platform.pos.y+platform.height);
+		if( joystick.y > 0 && (up == false || pos.y <= ground + 0.1) &&//clicked jump and within y bound
+			pos.x <= platform.pos.x + platform.width/2 && //within right bound
+			pos.x >= platform.pos.x - platform.width/2)//within left bound
+		{
+			if(jump_count == 0 || (double_jump && jump_count < 1)){
+				jump();
+			}
+			up = true;
+		}
+		if(joystick.y > 0) up = true;
+		else up = false;
 		if(within_bounds(pos, platform) && joystick.y <= 0) {
 			vel.y = 0;//transform.position.y = platform;
 			transform.Translate(0f, (float)(ground - pos.y), 0f);
@@ -99,16 +118,7 @@ public class MoveScript : MonoBehaviour {
 			transform.Translate((float)(center - pos.x), (float)(start_y - pos.y), 0f);
 			other.GetComponent<HealthScript>().TakeDamage(1);
 		}
-		if(
-			joystick.y > 0 && /*pos.y <= ground + 0.1 *///clicked jump and within y bound
-			pos.x <= platform.pos.x + platform.width/2 && //within right bound
-			pos.x >= platform.pos.x - platform.width/2)//within left bound
-		{
-			if(jump_count == 0 || (double_jump && jump_count <= 2)){
-				jump();
-			}
-		}
-		if(pos.y <= ground + 0.1){
+		if(pos.y <= ground){//+thresh?
 			jump_count = 0;
 			run(joystick.x);
 		}
@@ -119,13 +129,8 @@ public class MoveScript : MonoBehaviour {
 		vel.x = clamp(-max_vel, max_vel, vel.x);//clamped at max_vel m/s
 		vel.y = clamp(-3*max_vel, 3*max_vel, vel.y);//terminal velocity
 		if(abs(pos.x - pos2.x) <= 1 && abs(pos.y - pos2.y) <= 1){
-			collision((pos.y <= ground+0.5));
-			is_in = true;
-			if(pos.y > pos2.y + 0.1) {//jumps on head
-				gameObject.GetComponent<HealthScript>().TakeDamage(1);
-			}
-        	}	
-		else is_in = false;
+			collision(ground);
+        }
 		transform.Translate(
 			vel.x*Time.deltaTime, 
 			vel.y*Time.deltaTime, 
