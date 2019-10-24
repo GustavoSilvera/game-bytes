bad9a0975b68f0c03b26ef5910218d045e2d09b4
Gustavo <gustavo.r.silvera@gmail.com>
2019-10-24 19:39:18 -0400

    added SFX

---
+++ Crucible/Assets/Minigames/Sparty/Scripts/SFX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFX : MonoBehaviour
+{
+	// Start is called before the first frame update
+ 	public AudioSource boing;
+	public AudioSource mario;
+	public AudioSource stomp;
+	public AudioSource die;
+	public void PlayBoing(){
+		boing.Play();
+	}
+	public void PlayMario(){
+		mario.Play();
+	}
+	public void PlayStomp(){
+		stomp.Play();
+	}
+	public void PlayDie(){
+		die.Play();
+	}
+}
