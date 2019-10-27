fac2edec1c7ccd4a3129e6da6d9a62b8e1596f71
Gustavo <gustavo.r.silvera@gmail.com>
2019-10-27 14:18:45 -0400

    new canvas

--- Crucible/Assets/Minigames/Sparty/Scripts/ActionScript.cs
+++ Crucible/Assets/Minigames/Sparty/Scripts/ActionScript.cs
@@ -4,76 +4,76 @@ using UnityEngine;
 
 public class ActionScript : MonoBehaviour
 {
-    public GameObject projectile;
-    int player;
-    bool direction;
-    GameObject other;
+	public GameObject projectile;
+	int player;
+	bool direction;
+	GameObject other;
 
-    float cooldown = 0.5f;
-    float time = 0.0f;
+	float cooldown = 0.5f;
+	float time = 0.0f;
 
-    [SerializeField]
-    public float speed = 25.0f;
+	[SerializeField]
+	public float speed = 25.0f;
 
-    private void Start()
-    {
-        if (gameObject.name == "Player1")
-        {
-            player = 0;
-            other = GameObject.Find("Player2");
-        }
-        else
-        {
-            player = 1;
-            other = GameObject.Find("Player1");
-            direction = true;
-        }
+	private void Start()
+	{
+	    if (gameObject.name == "Player1")
+	    {
+	        player = 0;
+	        other = GameObject.Find("Player2");
+	    }
+	    else
+	    {
+	        player = 1;
+	        other = GameObject.Find("Player1");
+	        direction = true;
+	    }
 
-    }
+	}
 
-    void Update()
-    {
-        time += Time.deltaTime;
-        if (MinigameInputHelper.GetHorizontalAxis(player) < -0.1) direction = true; //left
-        else if (MinigameInputHelper.GetHorizontalAxis(player) > 0.1) direction = false; //right
-        if (MinigameInputHelper.IsButton1Down(player) && time >= cooldown)
-        {
-            time = 0;
-            GameObject p;
-            if (direction)
-            {
-                p = Instantiate(projectile, transform.position - new Vector3((float) 1, 0, 0), transform.rotation);
-                p.GetComponent<Rigidbody2D>().velocity = new Vector2(-25.0f, 10.0f);
-            }
-            else
-            {
-                p = Instantiate(projectile, transform.position + new Vector3((float) 1, 0, 0), transform.rotation);
-                p.GetComponent<Rigidbody2D>().velocity = new Vector2(25.0f, 10.0f);
-            }
-        }
-        if(MinigameInputHelper.IsButton2Down(player) && time >= cooldown)
-        {
-            double xdist = gameObject.transform.position.x - other.transform.position.x;
-            if (!direction && ((gameObject.transform.position.x < 0 && xdist > 0 && xdist < 2) ||
-                (gameObject.transform.position.x > 0 && xdist < 0 && xdist > -2)))
-            {
-                gameObject.GetComponent<HealthScript>().TakeDamage(1);
-            }
-            if(direction && ((gameObject.transform.position.x < 0 && xdist < 0 && xdist > -2) ||
-                (gameObject.transform.position.x > 0 && xdist > 0 && xdist < 2)))
-            {
-                gameObject.GetComponent<HealthScript>().TakeDamage(1);
-            }
-        }
-    }
+	void Update()
+	{
+	    time += Time.deltaTime;
+	    if (MinigameInputHelper.GetHorizontalAxis(player) < -0.1) direction = true; //left
+	    else if (MinigameInputHelper.GetHorizontalAxis(player) > 0.1) direction = false; //right
+	    if (MinigameInputHelper.IsButton1Down(player) && time >= cooldown)
+	    {
+	        time = 0;
+	        GameObject p;
+	        if (direction)
+	        {
+	            p = Instantiate(projectile, transform.position - new Vector3((float) 1, 0, 0), transform.rotation);
+	            p.GetComponent<Rigidbody2D>().velocity = new Vector2(-25.0f, 10.0f);
+	        }
+	        else
+	        {
+	            p = Instantiate(projectile, transform.position + new Vector3((float) 1, 0, 0), transform.rotation);
+	            p.GetComponent<Rigidbody2D>().velocity = new Vector2(25.0f, 10.0f);
+	        }
+	    }
+	    if(MinigameInputHelper.IsButton2Down(player) && time >= cooldown)
+	    {
+	        double xdist = gameObject.transform.position.x - other.transform.position.x;
+	        if (!direction && ((gameObject.transform.position.x < 0 && xdist > 0 && xdist < 2) ||
+	            (gameObject.transform.position.x > 0 && xdist < 0 && xdist > -2)))
+	        {
+	            gameObject.GetComponent<HealthScript>().TakeDamage(1);
+	        }
+	        if(direction && ((gameObject.transform.position.x < 0 && xdist < 0 && xdist > -2) ||
+	            (gameObject.transform.position.x > 0 && xdist > 0 && xdist < 2)))
+	        {
+	            gameObject.GetComponent<HealthScript>().TakeDamage(1);
+	        }
+	    }
+	}
 }
 
 /*public class Projectile : MonoBehaviour
 {
-    void OnCollisionEnter(Collision collision)
-    {
-        HealthScript c = (HealthScript)collision.gameObject.GetComponent(typeof(HealthScript));
-        c.TakeDamage();
-        Destroy(gameObject);
-    }
+	void OnCollisionEnter(Collision collision)
+	{
+	    HealthScript c = (HealthScript)collision.gameObject.GetComponent(typeof(HealthScript));
+	    c.TakeDamage();
+	    Destroy(gameObject);
+	}
 }*/
---
+++ Crucible/Assets/Minigames/Sparty/Scripts/MenuButtons.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelect : MonoBehaviour
+{
+	// Start is called before the first frame update
+	bool hover = false;
+	const float hoverScale = (float)1.5;
+	int current_place = 0;
+	GameObject p1Select, p2Select;
+
+	void Start()
+	{
+		p1Select = GameObject.Find("Player1Select");
+		p2Select = GameObject.Find("Player2Select");
+		//gameObject.name == "Player1"
+		//tennis = GameObject.Find("TennisButton");
+		//karate = GameObject.Find("KarateButton");
+		//baseball = GameObject.Find("BaseballButton");
+		if(this.name == "TennisButton"){
+			hover = true;
+			current_place = 0;
+		}if(this.name == "KarateButton"){
+			hover = false;
+			current_place = 1;
+		}if(this.name == "BaseballButton"){
+			hover = false;
+			current_place = 2;
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if(p1Select.GetComponent<MenuScript>().place == current_place 
+		|| p2Select.GetComponent<MenuScript>().place == current_place) hover = true;
+		else hover = false;
+		if(hover) transform.localScale += new Vector3(1, 1,1);//scale
+		transform.localScale = new Vector3(1,1,1);//scale
+
+	}
+}
