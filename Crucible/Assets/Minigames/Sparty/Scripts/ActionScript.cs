5d1e6231bf987d8e6b0c71d4a7710c2650867b61
Gustavo <gustavo.r.silvera@gmail.com>
2019-11-22 17:07:40 -0500

    bigger

--- Crucible/Assets/Minigames/Sparty/Scripts/ActionScript.cs
+++ Crucible/Assets/Minigames/Sparty/Scripts/ActionScript.cs
@@ -14,26 +14,26 @@ public class ActionScript : MonoBehaviour
 	GameObject other;
 
 	public bool defenseOn;
-    public float defCooldown = 4.0f;
-    //int shieldHits = 0;
+	public float defCooldown = 4.0f;
+	//int shieldHits = 0;
 
 	float cooldown = 0.5f;
 	float time = 0.0f;
 	int TYPE = 0;
-    [SerializeField]
-    public float speed = 25.0f;
-    SpriteRenderer spriteRenderer;
+	[SerializeField]
+	public float speed = 25.0f;
+	SpriteRenderer spriteRenderer;
 
-    Animator animator;
-    float attacktime = 0.0f;
+	Animator animator;
+	float attacktime = 0.0f;
 
-    int ballCharge = 0;
-    public int shieldHits;
+	int ballCharge = 0;
+	public int shieldHits;
 
-    private void Start()
+	private void Start()
 	{
-        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-        if (gameObject.name == "Player1"){
+	    spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+	    if (gameObject.name == "Player1"){
 			player = 0;
 			other = GameObject.Find("Player2");
 		}
@@ -47,13 +47,13 @@ public class ActionScript : MonoBehaviour
 		TYPE = this.GetComponent<MoveScript>().TYPE;//player type
 		Debug.Log(TYPE);
 
-        animator = gameObject.GetComponent<Animator>();
-        shieldHits = 0;
-    }
+	    animator = gameObject.GetComponent<Animator>();
+	    shieldHits = 0;
+	}
 
 	void Update(){
-        if (direction) spriteRenderer.flipX = false;
-        else spriteRenderer.flipX = true;
+	    if (direction) spriteRenderer.flipX = false;
+	    else spriteRenderer.flipX = true;
 		float horizontal_axis = MinigameInputHelper.GetHorizontalAxis(player);
 		bool button1 = MinigameInputHelper.IsButton1Down(player);
 		bool button2 = MinigameInputHelper.IsButton2Down(player);
@@ -62,128 +62,129 @@ public class ActionScript : MonoBehaviour
 		float pos_y = gameObject.transform.position.y;
 		float o_pos_x = other.transform.position.x;
 		float o_pos_y = other.transform.position.y;
-
+		//double p1_height = this.GetComponent<MoveScript>().playerHeight;
+		double p1_width = 0.6*this.GetComponent<MoveScript>().playerWidth;
 		time += Time.deltaTime;
 		if (horizontal_axis < -0.1) direction = true; //left
 		else if (horizontal_axis > 0.1) direction = false; //right
 
-        if ((button1 || MinigameInputHelper.IsButton1Held(player)) && time >= cooldown && TYPE != 4 && ballCharge < 30) ballCharge++;
+		if ((button1 || MinigameInputHelper.IsButton1Held(player)) && time >= cooldown && TYPE != 4 && ballCharge < 30) ballCharge++;
 
-        if (MinigameInputHelper.IsButton1Up(player) && time >= cooldown && ballCharge > 0 && TYPE != 4){
-            animator.SetInteger("state", 3 + TYPE);
-            attacktime = 0;
-            time = 0;
+		if (MinigameInputHelper.IsButton1Up(player) && time >= cooldown && ballCharge > 0 && TYPE != 4){
+		    animator.SetInteger("state", 3 + TYPE);
+		    attacktime = 0;
+		    time = 0;
 			GameObject p;
-            attacktime = 0;
-            if (direction && !gameObject.GetComponent<ActionScript>().defenseOn)
-            {
+		    attacktime = 0;
+		    if (direction && !gameObject.GetComponent<ActionScript>().defenseOn)
+		    {
 				p = Instantiate(projectile, transform.position - new Vector3((float)1, 0, 0), transform.rotation);
 				p.GetComponent<Rigidbody2D>().velocity = new Vector2(-1.25f, 0.5f) * ballCharge;
 
 			}
 			else if (!gameObject.GetComponent<ActionScript>().defenseOn)
-            {
+		    {
 				p = Instantiate(projectile, transform.position + new Vector3((float)1, 0, 0), transform.rotation);
 				p.GetComponent<Rigidbody2D>().velocity = new Vector2(1.25f, 0.5f) * ballCharge;
 			}
-            ballCharge = 0;
-        }
+		    ballCharge = 0;
+		}
 
-        if(attacktime > 0.5 && animator.GetInteger("state") == 3 + TYPE) animator.SetInteger("state", 0 + TYPE);
+		if(attacktime > 0.5 && animator.GetInteger("state") == 3 + TYPE) animator.SetInteger("state", 0 + TYPE);
 
-        if (button1 && time >= cooldown && TYPE != 0){//kick
+		if (button1 && time >= cooldown && TYPE != 0){//kick
 			double xdist = pos_x - o_pos_x;
-            /*if (!direction && ((pos_x < 0 && xdist > 0 && xdist < 2) ||
-                (pos_x > 0 && xdist < 0 && xdist > -2)) &&
-                other.GetComponent<ActionScript>().defenseOn)
-            {
-                other.GetComponent<ActionScript>().shieldHits = other.GetComponent<ActionScript>().shieldHits + 1;
-            }*/
-            if (!direction && ((pos_x < 0 && xdist > 0 && xdist < 2) ||
-				(pos_x > 0 && xdist < 0 && xdist > -2)))
+		    /*if (!direction && ((pos_x < 0 && xdist > 0 && xdist < 2) ||
+		        (pos_x > 0 && xdist < 0 && xdist > -2)) &&
+		        other.GetComponent<ActionScript>().defenseOn)
+		    {
+		        other.GetComponent<ActionScript>().shieldHits = other.GetComponent<ActionScript>().shieldHits + 1;
+		    }*/
+			if (!direction && ((pos_x < 0 && xdist > 0 && xdist < 2*p1_width) ||
+				(pos_x > 0 && xdist < 0 && xdist > -2*p1_width)))
 			{
-                if (!other.GetComponent<ActionScript>().defenseOn)
-                {
-                    gameObject.GetComponent<HealthScript>().TakeDamage(1);
-                    Debug.Log("reached here");
-                }
-                else
-                {
-                    shieldHits++;
-                    Debug.Log(shieldHits);
-                    if (shieldHits >= 3)
-                    {
-                        shield.transform.localScale = new Vector3(0, 0, 0);
-                        defenseOn = false;
-                        shieldHits = 0;
-                        defCooldown = 0;
-                    }
-                }
+		        if (!other.GetComponent<ActionScript>().defenseOn)
+		        {
+		            gameObject.GetComponent<HealthScript>().TakeDamage(1);
+		            Debug.Log("reached here");
+		        }
+		        else
+		        {
+		            shieldHits++;
+		            Debug.Log(shieldHits);
+		            if (shieldHits >= 3)
+		            {
+		                shield.transform.localScale = new Vector3(0, 0, 0);
+		                defenseOn = false;
+		                shieldHits = 0;
+		                defCooldown = 0;
+		            }
+		        }
 			}
 
-            /*if (direction && ((pos_x < 0 && xdist < 0 && xdist > -2) ||
-                (pos_x > 0 && xdist > 0 && xdist < 2)) &&
-                other.GetComponent<ActionScript>().defenseOn)
-            {
-                other.GetComponent<ActionScript>().shieldHits++;
-            }*/
-            if (direction && ((pos_x < 0 && xdist < 0 && xdist > -2) ||
-                (pos_x > 0 && xdist > 0 && xdist < 2)))
+		    /*if (direction && ((pos_x < 0 && xdist < 0 && xdist > -2) ||
+		        (pos_x > 0 && xdist > 0 && xdist < 2)) &&
+		        other.GetComponent<ActionScript>().defenseOn)
+		    {
+		        other.GetComponent<ActionScript>().shieldHits++;
+		    }*/
+		    if (direction && ((pos_x < 0 && xdist < 0 && xdist > -2*p1_width) ||
+		        (pos_x > 0 && xdist > 0 && xdist < 2*p1_width)))
 			{
-                if (!other.GetComponent<ActionScript>().defenseOn)
-                {
-                    gameObject.GetComponent<HealthScript>().TakeDamage(1);
-                    Debug.Log("reached here");
-                }
+		        if (!other.GetComponent<ActionScript>().defenseOn)
+		        {
+		            gameObject.GetComponent<HealthScript>().TakeDamage(1);
+		            Debug.Log("reached here");
+		        }
 				else
-                {
-                    shieldHits++;
-                    Debug.Log(shieldHits);
-                    if (shieldHits >= 3)
-                    {
-                        shield.transform.localScale = new Vector3(0, 0, 0);
-                        defenseOn = false;
-                        shieldHits = 0;
-                        defCooldown = 0;
-                    }
-                }
+		        {
+			        shieldHits++;
+		            Debug.Log(shieldHits);
+		            if (shieldHits >= 3)
+		            {
+		                shield.transform.localScale = new Vector3(0, 0, 0);
+		                defenseOn = false;
+		                shieldHits = 0;
+		                defCooldown = 0;
+		            }
+		        }
 			}
 		}
-        if (button1 && TYPE != 0) {
-            Debug.Log("karate attack");
-            animator.SetInteger("state", 3 + TYPE);
-            Debug.Log(animator.GetInteger("state"));
-            attacktime = 0;
-        }
+		if (button1 && TYPE != 0) {
+		    Debug.Log("karate attack");
+		    animator.SetInteger("state", 3 + TYPE);
+		    Debug.Log(animator.GetInteger("state"));
+		    attacktime = 0;
+		}
 		if (button1 && time >= cooldown && TYPE != 0 && !other.GetComponent<ActionScript>().defenseOn){//punch //IS THERE A SHIELD CHECK HERE?? yes!
-            double xdist = pos_x - o_pos_x;
-            if (!direction && ((pos_x < 0 && xdist > 0 && xdist < 2) ||
-				(pos_x > 0 && xdist < 0 && xdist > -2)))
+		    double xdist = pos_x - o_pos_x;
+		    if (!direction && ((pos_x < 0 && xdist > 0 && xdist < 2*p1_width) ||
+				(pos_x > 0 && xdist < 0 && xdist > -2*p1_width)))
 			{
 				gameObject.GetComponent<HealthScript>().TakeDamage(1);
 			}
-			if (direction && ((pos_x < 0 && xdist < 0 && xdist > -2) ||
-				(pos_x > 0 && xdist > 0 && xdist < 2)))
+			if (direction && ((pos_x < 0 && xdist < 0 && xdist > -2*p1_width) ||
+				(pos_x > 0 && xdist > 0 && xdist < 2*p1_width)))
 			{
 				gameObject.GetComponent<HealthScript>().TakeDamage(1);
 			}
 		}
-        defCooldown += Time.deltaTime;
+		defCooldown += Time.deltaTime;
 		if (button2 && TYPE != 8 && defCooldown > 3){
 			shield.transform.localScale = new Vector3(1, 1, 1);
 			defenseOn = true;
-            defCooldown = 0;
+		    defCooldown = 0;
 		}
-        /*if(shieldHits > 3)
-        {
-            defenseOn = false;
-            shieldHits = 0;
-            defCooldown = 0;
-        }*/
+		/*if(shieldHits > 3)
+		{
+		    defenseOn = false;
+		    shieldHits = 0;
+		    defCooldown = 0;
+		}*/
 		if (button2Up){
 			shield.transform.localScale = new Vector3(0, 0, 0);
 			defenseOn = false;
 		}
-        attacktime += Time.deltaTime;
-    }
+		attacktime += Time.deltaTime;
+	}
 }
--- Crucible/Assets/Minigames/Sparty/Scripts/Projectile.cs
+++ Crucible/Assets/Minigames/Sparty/Scripts/Projectile.cs
@@ -19,84 +19,90 @@ public class Projectile : MonoBehaviour
 
 	private void OnEnable()
 	{
-	    rb = GetComponent<Rigidbody2D>();
-	    rb.velocity = initialVelocity;
+		rb = GetComponent<Rigidbody2D>();
+		rb.velocity = initialVelocity;
 	}
 
 	private void Update()
 	{
-	    lastFrameVelocity = rb.velocity;
+		lastFrameVelocity = rb.velocity;
 
-	    double xdist = gameObject.transform.position.x - p1.transform.position.x;
-	    double ydist = gameObject.transform.position.y - p1.transform.position.y;
-	    if (xdist > -0.5 && xdist < 0.5 && ydist < 0.5 && ydist > -0.5)
-	    {
-            if (!p1.GetComponent<ActionScript>().defenseOn)
-            {
-                p2.GetComponent<HealthScript>().TakeDamage(1);
-            }
-            else
-            {
-                p1.GetComponent<ActionScript>().shieldHits++;
-                if (p1.GetComponent<ActionScript>().shieldHits >= 3)
-                {
-                    p1.GetComponent<ActionScript>().shield.transform.localScale = new Vector3(0, 0, 0);
-                    p1.GetComponent<ActionScript>().defenseOn = false;
-                    p1.GetComponent<ActionScript>().shieldHits = 0;
-                    p1.GetComponent<ActionScript>().defCooldown = 0;
-                }
-            }
-            Destroy(gameObject);
+		double xdist = gameObject.transform.position.x - p1.transform.position.x;
+		double ydist = gameObject.transform.position.y - p1.transform.position.y;
+		double p1_height = p1.GetComponent<MoveScript>().playerHeight;
+		double p1_width = p1.GetComponent<MoveScript>().playerWidth;
+		
+		if (xdist > -p1_width/2 && xdist < p1_width/2 && ydist < p1_height/2 && ydist > -p1_height/2)
+		{
+	        if (!p1.GetComponent<ActionScript>().defenseOn)
+	        {
+	            p2.GetComponent<HealthScript>().TakeDamage(1);
+	        }
+	        else
+	        {
+	            p1.GetComponent<ActionScript>().shieldHits++;
+	            if (p1.GetComponent<ActionScript>().shieldHits >= 3)
+	            {
+	                p1.GetComponent<ActionScript>().shield.transform.localScale = new Vector3(0, 0, 0);
+	                p1.GetComponent<ActionScript>().defenseOn = false;
+	                p1.GetComponent<ActionScript>().shieldHits = 0;
+	                p1.GetComponent<ActionScript>().defCooldown = 0;
+	            }
+	        }
+	        Destroy(gameObject);
 
 		}
-	    xdist = gameObject.transform.position.x - p2.transform.position.x;
-	    ydist = gameObject.transform.position.y - p2.transform.position.y;
-	    if (xdist > -0.5 && xdist < 0.5 && ydist < 0.5 && ydist > -0.5)
-	    {
-            if (!p2.GetComponent<ActionScript>().defenseOn)
-            {
-                p1.GetComponent<HealthScript>().TakeDamage(1);
-            }
-            else
-            {
-                p2.GetComponent<ActionScript>().shieldHits++;
-                if (p2.GetComponent<ActionScript>().shieldHits >= 3)
-                {
-                    p2.GetComponent<ActionScript>().shield.transform.localScale = new Vector3(0, 0, 0);
-                    p2.GetComponent<ActionScript>().defenseOn = false;
-                    p2.GetComponent<ActionScript>().shieldHits = 0;
-                    p2.GetComponent<ActionScript>().defCooldown = 0;
-                }
-            }
+		xdist = gameObject.transform.position.x - p2.transform.position.x;
+		ydist = gameObject.transform.position.y - p2.transform.position.y;
+		double p2_width = p2.GetComponent<MoveScript>().playerHeight;
+		double p2_height = p2.GetComponent<MoveScript>().playerWidth;
+		
+		if (xdist > -p2_width/2 && xdist < p2_width/2 && ydist < p2_height/2 && ydist > -p2_height/2)
+		{
+	        if (!p2.GetComponent<ActionScript>().defenseOn)
+	        {
+	            p1.GetComponent<HealthScript>().TakeDamage(1);
+	        }
+	        else
+	        {
+	            p2.GetComponent<ActionScript>().shieldHits++;
+	            if (p2.GetComponent<ActionScript>().shieldHits >= 3)
+	            {
+	                p2.GetComponent<ActionScript>().shield.transform.localScale = new Vector3(0, 0, 0);
+	                p2.GetComponent<ActionScript>().defenseOn = false;
+	                p2.GetComponent<ActionScript>().shieldHits = 0;
+	                p2.GetComponent<ActionScript>().defCooldown = 0;
+	            }
+	        }
 			Destroy(gameObject);
 		}
 	}
 	private void Start()
 	{
-	    gameObject.GetComponent<Rigidbody2D>().gravityScale = 5;
-	    p1 = GameObject.Find("Player1");
-	    p2 = GameObject.Find("Player2");
+		gameObject.GetComponent<Rigidbody2D>().gravityScale = 5;
+		p1 = GameObject.Find("Player1");
+		p2 = GameObject.Find("Player2");
 	}
 
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
-	    Bounce(collision.contacts[0].normal);
+		Bounce(collision.contacts[0].normal);
 	}
 
 	private void Bounce(Vector2 collisionNormal)
 	{
-	    var speed = lastFrameVelocity.magnitude;
-	    var direction = Vector2.Reflect(lastFrameVelocity.normalized, collisionNormal);
+		var speed = lastFrameVelocity.magnitude;
+		var direction = Vector2.Reflect(lastFrameVelocity.normalized, collisionNormal);
 
-	    Debug.Log("Out Direction: " + direction);
-	    rb.velocity = direction * Mathf.Max(speed, minVelocity);
+		Debug.Log("Out Direction: " + direction);
+		rb.velocity = direction * Mathf.Max(speed, minVelocity);
 	}
 
 	//End of Collision Stuff
 	
 	void OnBecameInvisible()
 	{
-	    Destroy(gameObject);
+		Destroy(gameObject);
 	}
 
 }
