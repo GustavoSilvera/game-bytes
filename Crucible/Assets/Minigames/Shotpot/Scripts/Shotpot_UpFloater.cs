4db69f63131ef901c4943403830b0c013888d904
GustavoSilvera <gustavo@uap-mcohen3-l.wv.cc.cmu.edu>
2019-12-05 17:20:11 -0500

    Fixed Duplicates in Crucible

---
+++ Crucible/Assets/Minigames/Shotpot/Scripts/Shotpot_UpFloater.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shotpot_UpFloater : MonoBehaviour
+{
+    [SerializeField] float speed = 0.0f;
+    [SerializeField] float acceleration = 0.0f;
+    [SerializeField] float upRandomMod = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    speed = speed + Random.Range(-upRandomMod,upRandomMod);	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    transform.position = transform.position + new Vector3(0,speed * Time.deltaTime,0);
+        speed += acceleration * Time.deltaTime;	
+	}
+}
